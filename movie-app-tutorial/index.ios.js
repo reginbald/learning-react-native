dde765942148d781dca05535ef3e30608ed250e4
reginbald <mail@reginbald.com>
2017-05-05 19:21:15 +0200

    Added styling

--- movie-app-tutorial/index.ios.js
+++ movie-app-tutorial/index.ios.js
@@ -22,12 +22,14 @@ class Project extends Component {
     var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
         <Image 
           source={{uri: movie.posters.thumbnail}} 
           style={styles.thumbnail}
           />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -36,13 +38,25 @@ class Project extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+  rightContainer: {
+    flex: 1,
+  },
   thumbnail: {
     width: 53,
     height: 81,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
   },
 });
 
